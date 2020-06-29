81f256fbb20abd517bb1358c7e43b98a63046f54
Verusca silva <56118750+Vesilva33@users.noreply.github.com>
2020-06-29 15:46:52 -0300

    Add files via upload
    dados do curso

---
+++ main.js
@@ -0,0 +1,121 @@
+
+function trocar(elemento){
+  //document.getElementById("mousemove").innerHTML ="obrigado por passar o mouse";
+  elemento.innerHTML="obrigado por passar o mouse";
+}
+
+function voltar(elemento){
+  //document.getElementById("mousemove").innerHTML ="passe o mouse aqui";
+  elemento.innerHTML="passe o mouse aqui";
+}
+
+function load()
+{
+  alert("pagina carregada");
+}
+function funcaoChange(elemento){
+  console.log(elemento.value);
+}
+
+
+/*function clique(){
+  document.getElementById("agradecimento").innerHTML = "obrigado por clicar";
+  //console.log(document.getElementById("agradecimento"));
+}
+function redirecionar(){
+  window.open("http://google.com.br/");
+  window.location.href =" http://google.com.br/";
+
+}
+*/
+
+
+
+/*function soma(n1 , n2){
+  return n1 + n2;
+}
+function setReplace(frase ,nome ,novo_nome){
+  return frase.replace(nome, novo_nome)
+}
+
+alert(soma(5,10));
+alert(setReplace("vai japão","japão","brasil"));
+
+*/
+
+
+
+
+
+/*let dt= new Date;
+Alert(dt);
+*/
+
+
+
+
+
+
+/*var count;
+for(count=1;count<=5;count++){
+    alert(count);
+};
+
+*/
+
+
+/*var count = 0;
+while(count<=5){
+    console.log(count);
+    alert(count);
+    count ++;
+}
+*/
+
+
+
+
+
+//varidade= prompt('qual sua idade');
+//if(idade>18)
+//{
+//    alert('maior de idade');
+
+//}else{
+  //  alert('menor de idade');
+//}
+
+
+
+
+
+/*var frutas=[{nome:"maça",cor:"vermelha"},{nome:"uva",cor:"roxa"}]
+console.log(frutas);
+alert(frutas[1].nome);*/
+
+
+
+
+
+/*var fruta={nome:"maça",cor:"vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+
+//var nome = "verusca schimmels";
+//var idade = 31;
+//var idade2 = 23;
+//alert(nome+"meu primeiro "+ idade +"minha idade");
+//var frase="japao e melhor time do mundo";
+//console.log(nome);
+//console.log(idade +idade2);
+//console.log(frase.replace("japao","brasil"));
+//alert(frase.replace("japao","brasil"));
+
+//var lista = ["maça", "pêra","laranja"];
+//lista.push("uva");
+//lista .pop();
+//console.log(lista.reverse);
+//console.log(lista.topString());
+//console.log(lista.join(" "));
